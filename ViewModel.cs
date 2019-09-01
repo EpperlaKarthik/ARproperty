7000687011eee77164fe1014d7739bd3a31cdef8
EpperlaKarthik <52861755+EpperlaKarthik@users.noreply.github.com>
2019-09-01 16:02:25 +0530

    Add files via upload

---
+++ ViewModel.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ViewModel : MonoBehaviour 
+{
+    public Text buttonText;
+    public GameObject SecondFloor;
+    
+    public Slider slider;
+
+    public void Slider_Changed(float newValue)
+    {
+        Vector3 pos = SecondFloor.transform.position;
+        pos.y = newValue;
+        SecondFloor.transform.position = pos;
+    }
+
+    public void Button_Click()
+    {
+        Debug.Log("Hello, World!");
+    }
+
+    public void Button_String(string msg)
+    {
+        buttonText.text = msg;
+    }
+
+    public void Toggle_Changed(bool newValue)
+    {
+        SecondFloor.SetActive(newValue);
+        slider.interactable = newValue;
+    }
+
+    public void Text_Changed(string newText)
+    {
+        float temp = float.Parse(newText);
+        SecondFloor.transform.localScale = new Vector3(temp, temp, temp);
+    }
+
+
+
+
+}
